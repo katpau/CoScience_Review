6bbce9392a50077cc8272751929efd4ac06228ff
ckuehrt <90376128+ckuehrt@users.noreply.github.com>
2023-04-28 11:22:16 +0200

    refinded CEI calculation
    added preprocessing of variables, checks of model assumptions

--- Only_ForGit_To_TestRun/Questionnaire_Prep/Fork_Questionnaire_Common.R
+++ Only_ForGit_To_TestRun/Questionnaire_Prep/Fork_Questionnaire_Common.R
@@ -263,6 +263,232 @@ for (CutOff_Applied in 0) { # Cut loop here for testing
     
     ## 2.5 Calculate CFA for Cognitive Effort Investment #######################
     
+    if (TaskName == "Flanker_Conflict") {
+      
+      # 1. requirements: 
+      # TODO @Kat: for this, we need 
+        # "NFC_NeedForCognition"
+        # "BSCS_SelfControl"
+        # "ATQ_EffortfulControl"
+        # "TIF_Intellect"
+        # as sum score for each participant
+      
+      # 2. determine deviation from univariate normality using Shapiro Wilk test
+        # use this function (TODO @Kat maybe to be placed elsewhere)
+        # wrapper for shapiro.test()
+        sw <- function(x) {
+          x = data.frame(x)
+          d = dim(x)
+          n = names(x)
+          r = NULL
+          for (i in 1:dim(x)[2]) {
+            y = shapiro.test(x[, i])
+            v = cbind(y$statistic, y$p.value)
+            colnames(v) = c("W", "p")
+            rownames(v) = n[i]
+            r = rbind(r, v)
+          }
+          return(round(r, 3))
+        }
+      
+      shapiro.summary = rbind(cbind("NFC", sw(Score_Data_AllScores$NFC_NeedForCognition)), 
+                              cbind("BSCS",sw(Score_Data_AllScores$BSCS_SelfControl)),
+                              cbind("EC",sw(Score_Data_AllScores$ATQ_EffortfulControl)),
+                              cbind("TIF",sw(Score_Data_AllScores$TIF_Intellect)))
+ 
+
+      # 3. normalize and standardize sum scores by Blom's formula (1954)
+        # use this function (TODO @Kat maybe to be placed elsewhere)
+        # for normalizing variables
+        normalize.gen <- function(data, whichFormula = 2, whichMeanSD = 1) {
+          # normalizes DATA using one of the formulae:
+          #
+          # 1 = Van der Waerden (1952): r * / (n + 1)
+          # 2 = Blom (1954):           (r - 3/8) / (n + 1/4) (default)
+          # 3 = Bliss (1956; Rankit):  (r - 1/2) / n
+          # 4 = Tukey (1962):          (r - 1/3) / (n + 1/3)
+          #
+          # Ties will be treated as average, and the output will be rescaled
+          # to have mean = 0 and sd = 1, if you enter 1 for mean0.sd1 (default);
+          # if you want to keep the mean and sd of the original variable, enter 0.
+          
+          normalize = function(data, normalize.formula = whichFormula,
+                               mean0.sd1 = whichMeanSD) {
+            r = rank(data,na.last = "keep",ties.method = ("average"))
+            n = sum(!is.na(r))
+            
+            if (normalize.formula == 1)
+              x = r / (n + 1)
+            if (normalize.formula == 2)
+              x = (r - 3 / 8) / (n + 1 / 4)
+            if (normalize.formula == 3)
+              x = (r - 1 / 2) / n
+            if (normalize.formula == 4)
+              x = (r - 1 / 3) / (n + 1 / 3)
+            
+            if (mean0.sd1 == 1)
+              normal = qnorm(
+                x,
+                mean = 0,
+                sd = 1,
+                lower.tail = TRUE,
+                log.p = FALSE
+              )
+            else
+              normal = qnorm(
+                x,
+                mean = mean(data, na.rm = T),
+                sd = sd(data, na.rm = T),
+                lower.tail = TRUE,
+                log.p = FALSE
+              )
+          }
+          
+          if (class(data) == "numeric") {
+            normal = normalize(data, whichFormula, whichMeanSD)
+          } else {
+            normal = NULL
+            for (i in 1:dim(data)[2]) {
+              normal = cbind(normal, normalize(data[, i], whichFormula, whichMeanSD))
+            }
+            colnames(normal) = names(data)
+          }
+          
+          return(normal)
+          
+        }
+      
+      
+      # 4. calculate normalized sum scores, rename (for easier use) and save in separate object in one step
+        # TODO @ Kat maybe adjust df name
+      nfc = data.frame(normalize.gen(data.frame(Score_Data_AllScores$NFC_NeedForCognition)))
+      scs = data.frame(normalize.gen(data.frame(Score_Data_AllScores$BSCS_SelfControl)))
+      int = data.frame(normalize.gen(data.frame(Score_Data_AllScores$TIF_Intellect)))
+      eco = data.frame(normalize.gen(data.frame(Score_Data_AllScores$ATQ_EffortfulControl)))
+     
+      
+      # 5. determine deviation from univariate normality using Shapiro Wilk test
+      shapiro.summary = rbind(shapiro.summary, 
+                              cbind("NFC.normal", sw(nfc)), 
+                              cbind("BSCS.normal",sw(scs)),
+                              cbind("EC.normal", sw(eco)),
+                              cbind("TIF.normal",sw(int)))
+      # save results 
+      write.csv2(shapiro.summary, file = paste0(Root, "/Test-Flanker_Conflict/CEI_shapiro.test.summary.csv")) # TODO @ Kat maybe to be adjusted
+
+      
+      # 6. perform Mardia test in order to check for deviation from multivariate normality
+      # create new data frame
+      d.1 = data.frame(nfc, scs, int, eco)
+      colnames(d.1) = c("nfc", "scs", "int", "eco")
+      
+      # psych package is needed
+      library(psych)
+      mardia.CEI = mardia(d.1)
+      # if both p-values of skewness and kurtosis statistics are greater than .05
+      # > multivariate normality is given
+      
+      # 7. perform cfa
+      ## set up the hierarchical model
+      m1 = '
+      COM =~ nfc + int                                   # Cognitive Motivation
+      ESC =~ scs + eco                                   # Effortful Self-Control
+      CEI =~ COM + ESC + 0*nfc + 0*int + 0*scs + 0*eco   # Cognitive Effort Investment, equal loadings freely estimated
+      COM ~~ psi*COM                                     # residuals of COM and ESC set equal
+      ESC ~~ psi*ESC
+      nfc ~~ eps*nfc                                     # error variances of indicators set equal
+      int ~~ eps*int
+      scs ~~ eps*scs
+      eco ~~ eps*eco
+      '
+      
+      # calculate cfa, get summary and report
+      # lavaan package is needed
+      library(lavaan)
+      # if mardia not sign. (multivariate normality is given) --> use ML as estimator, else MLR
+      f1 = cfa(m1, data = d.1, estimator = "MLR")   # TODo @ Kat, adjust according to mardia results
+      summary(f1, standardized = T, fit.measures = T, nd = 3)
+      
+      
+      # TODO @Kat, we can use the function below to report model fit, maybe you have a better place to put it than right here?
+      # required function for reporting model fit
+        fm.report <- function(fit, scaled = T) {
+          # which fit measures
+          if (scaled == T) {
+            fm = fitmeasures(fit)[c(
+              "chisq.scaled",
+              "df.scaled",
+              "pvalue.scaled",
+              "cfi.scaled",
+              "rmsea.scaled",
+              "rmsea.ci.lower.scaled",
+              "rmsea.ci.upper.scaled",
+              "srmr"
+            )]
+          } else {
+            fm = fitmeasures(fit)[c(
+              "chisq",
+              "df",
+              "pvalue",
+              "cfi",
+              "rmsea",
+              "rmsea.ci.lower",
+              "rmsea.ci.upper",
+              "srmr"
+            )]
+          }
+          # format
+          fmr <- function(fm, digits = 2, nsmall = 2, gt1 = T, pval = F) {
+            fm.out = format(round(fm, digits), nsmall = nsmall)
+            if (gt1 == F) {
+              fm.out = sub("0.", ".", fm.out)
+            }
+            if (pval == T & fm.out == ".000") {
+              fm.out = "< .001"
+            } else if (pval == T & fm.out != ".000") {
+              fm.out = paste0("= ", fm.out)
+            }
+            return(fm.out)
+          }
+          # collapse
+          fm.txt = paste0(
+            "chi2 = ",
+            fmr(fm[1]),
+            ", df = ",
+            fmr(fm[2], 0, 0),
+            ", p ",
+            fmr(fm[3], 3, 3, gt1 = F, pval = T),
+            ", CFI = ",
+            fmr(fm[4], gt1 = F),
+            ", RMSEA = ",
+            fmr(fm[5], gt1 = F),
+            " with 90% CI [",
+            fmr(fm[6], gt1 = F),
+            ", ",
+            fmr(fm[7], gt1 = F),
+            "], ",
+            "SRMR = ",
+            fmr(fm[8], gt1 = F)
+          )
+          # return
+          return(fm.txt)
+        }
+        
+      fm.report(f1, scaled = F)
+      # [1] "chi2 = 32.66, df = 4, p < .001, CFI = .67, RMSEA = .27 with 90% CI [.19, .36], SRMR = .14"
+      
+      # reliability (McDonald's Omega)
+      CEI_omegas = round(reliability(f1)['omega3', ], 2)[c(3, 1, 2)]
+      
+      
+      ## extract factor scores from cfa
+      fac.scores <- data.frame(lavPredict(f1, type = "lv"))
+      
+      # TODO @Kat save within export data frame (each variable or by cbind) & add to "merge and export data"
+      df.pq$ESC <- fac.scores$ESC
+      df.pq$COM <- fac.scores$COM
+      df.pq$CEI <- fac.scores$CEI
+    }
     
 
     ## 2.6 Get Scores for the Personality Variable #############################
