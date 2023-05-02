9cae3d396d5b84c56bbd0359bd8d26c98244a7c7
ckuehrt <90376128+ckuehrt@users.noreply.github.com>
2023-05-02 11:20:07 +0200

    distinguish 2 centering methods for LMM

--- Step_Functions/Statistics_Flanker_Conflict/Center.R
+++ Step_Functions/Statistics_Flanker_Conflict/Center.R
@@ -6,10 +6,21 @@ Center = function(input = NULL, choice = NULL) {
   
   ## Contributors
   # Last checked by KP 12/22
-  # Planned/Completed Review by:
+  # Planned/Completed Review by: CK 05/23
 
   # Handles all Choices listed above 
   # Normalizes the predictors (e.g. behavioral and personality) data within all conditions
+  
+  # CK: we might have two conditions here
+    # (1) center level 1 predictors within cluster (CWC), i.e. group mean centering
+          #  demand, electrode, block
+    sub.rt$demand.cwc <- sub.rt$demand - ave(sub.rt$demand, sub.rt$id_t2, FUN = function(x) mean(x, na.rm = T))
+    # (2) center level 2 predictors at the grand mean (CGM)
+          # CEI, COM, ESC, fluidIntelligence, age, gender, depression, anxiety, O, C, E, A, N
+    sub.rt$CEI.cgm <- scale(sub.rt$CEI, scale = F)
+  
+    
+  
   # (1) Center Personality Variables
   
   if (choice == "Centered")  {
