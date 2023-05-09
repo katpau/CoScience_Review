e15d634661a980d48d523ff16f14367098a5e04e
eporth <elisa.porth@uni-koeln.de>
2023-05-09 16:13:21 +0200

    Changed prep_lrp
    Changed to recode LRP for errors and then merge correct responses and errors

--- Analysis_Functions/Error_MVPA/LRP/prep_lrp.m
+++ Analysis_Functions/Error_MVPA/LRP/prep_lrp.m
@@ -45,178 +45,148 @@ for part = 1:length(participant_codes)
     %find indices from left and righz hemisphere electrodes to ensure that 
     %right hemisphere electrode acitivty is substracted from the left later on
     chanlocs_remain = struct2table(EEG_data.Data.data.EEG.chanlocs);
-    left_hemisphere = find(strcmp(chanlocs_remain.labels, Electrodes(1)));
-    right_hemisphere = find(strcmp(chanlocs_remain.labels, Electrodes(2)));
+    C3 = find(strcmp(chanlocs_remain.labels, Electrodes(1)));
+    C4 = find(strcmp(chanlocs_remain.labels, Electrodes(2)));
     
     clear Electrodes chanlocs_remain
 
-    %create matrix for LRP (dp x trials)
+    % Create vector containing the response types
+    events = struct2table(EEG_data.Data.data.EEG.event);
+    events_response = events(strcmp(events.Event, 'Response'),:);
+    
+    if AnalysisName == "Flanker_MVPA"
+        for i = 1:(height(events_response))
+            if events_response.ACC(i) == 1
+               events_response.responsetype(i) = 1;
+            elseif events_response.ACC(i) == 0
+               events_response.responsetype(i) = 2;
+            else 
+               events_response.responsetype(i) = 0;    
+            end
+        end
+    elseif AnalysisName == "GoNoGo_MVPA" 
+        for i = 1:(height(events_response))
+            if strcmp(events_response.Type(i), 'Go') && events_response.ACC(i) == 1
+            events_response.responsetype(i) = 1;
+            elseif strcmp(events_response.Type(i), 'NoGo') && events_response.ACC(i) == 0
+            events_response.responsetype(i) = 2;
+            else 
+            events_response.responsetype(i) = 0;    
+            end
+        end
+    end
+
+    %create matrix for LRP (dp x trials) separately for both response types
     EEG_data.Data.data.EEG.data_lrp = zeros(length(EEG_data.Data.data.EEG.data(1,:,1)), length(EEG_data.Data.data.EEG.data(1,1,:)));
     for i = 1:length(EEG_data.Data.data.EEG.data(1,1,:)) % number of trials
         for ii = 1:length(EEG_data.Data.data.EEG.data(1,:,1)) % number of data points
-            EEG_data.Data.data.EEG.data_lrp(ii,i) = EEG_data.Data.data.EEG.data(left_hemisphere,ii,i) - EEG_data.Data.data.EEG.data(right_hemisphere,ii,i); 
+            EEG_data.Data.data.EEG.data_lrp_correct(ii,i) = EEG_data.Data.data.EEG.data(C3,ii,i) - EEG_data.Data.data.EEG.data(C4,ii,i); 
+            EEG_data.Data.data.EEG.data_lrp_error(ii,i) = EEG_data.Data.data.EEG.data(C4,ii,i) - EEG_data.Data.data.EEG.data(C3,ii,i); %substract C3 from C4 for error trials
         end
     end
-    clear i ii left_hemisphere right_hemisphere
-
-    %create participant average across trials (dp)
-    EEG_data.Data.data.EEG.data_lrp_aver = zeros(length(EEG_data.Data.data.EEG.data_lrp(:,1)),1);
-    for i = 1:length(EEG_data.Data.data.EEG.data_lrp(:,1))
-        EEG_data.Data.data.EEG.data_lrp_aver(i) = mean(EEG_data.Data.data.EEG.data_lrp(i,:));
-    end
-
-    clear i
+    clear i ii C3 C4
+ 
+    EEG_data.Data.data.EEG.data_lrp_correct = EEG_data.Data.data.EEG.data_lrp_correct(:, events_response.responsetype == 1); %correct trials only
+    EEG_data.Data.data.EEG.data_lrp_error = EEG_data.Data.data.EEG.data_lrp_error(:, events_response.responsetype == 2); %error trials only
+    data_lrp = [EEG_data.Data.data.EEG.data_lrp_correct EEG_data.Data.data.EEG.data_lrp_error]; %merge correct and error trials 
     
-    %add vector with ms
-    ms_start = EEG_data.Data.data.EEG.xmin*1000; %start of epoch 
-    ms_end = EEG_data.Data.data.EEG.xmax*1000;  %end of epoch
-
-    % add code for sampling rate 512 Hz here, something like
-    % if sampling == 512
-        % ms_down = [ms_start:1.95:ms_end]'; 
-        % ms_base =  [ms_start:2:ms_end]';
-        % ms_base = [ms_base, zeroes(length(ms_down)-length(ms_base))]';
-        % ms = [ms_down, ms_base];
-        % ms.diff = ms.down-ms.base;
-        % for i = 1:length(ms.down)
-        % if ms.diff(i) > 1
-            % ms.down(i)=[];
+    %only continue if data set contains any error trials
+    if isempty(EEG_data.Data.data.EEG.data_lrp_error)
+        fprintf('There are no error trials in the data set. Continuing with next participant.');
+        ErrorMessage = "Zero error trials.";
+        FileName = strcat(output_dir, 'logs/', "Error_", part_code, ".txt");
+        fid = fopen(FileName, 'wt');
+        fprintf(fid, ErrorMessage);
+        fclose(fid);
+    else
+        %create participant average across trials (dp) 
+        EEG_data.Data.data.EEG.data_lrp_aver = zeros(length(data_lrp(:,1)),1);
+        for i = 1:length(data_lrp(:,1))
+            EEG_data.Data.data.EEG.data_lrp_aver(i) = mean(data_lrp(i,:));
+        end
+        clear i
+    
+        %add vector with ms
+        ms_start = EEG_data.Data.data.EEG.xmin*1000; %start of epoch 
+        ms_end = EEG_data.Data.data.EEG.xmax*1000;  %end of epoch
+    
+        % add code for sampling rate 512 Hz here, something like
+        % if sampling == 512
+            % ms_down = [ms_start:1.95:ms_end]'; 
+            % ms_base =  [ms_start:2:ms_end]';
+            % ms_base = [ms_base, zeroes(length(ms_down)-length(ms_base))]';
+            % ms = [ms_down, ms_base];
             % ms.diff = ms.down-ms.base;
-        % end
-        %end
-        %else 
-        %...
-        %end
+            % for i = 1:length(ms.down)
+            % if ms.diff(i) > 1
+                % ms.down(i)=[];
+                % ms.diff = ms.down-ms.base;
+            % end
+            %end
+            %else 
+            %...
+            %end
+    
+        ms = [ms_start:2:ms_end]'; %create ms vector with time steps of 2 ms
+        EEG_data.Data.data.EEG.data_lrp_aver(:,2) = ms;
+        
+        %add vector with id
+        id = cell2table(part_code);
+        id = repmat(id,length(ms),1);
+        EEG_data.Data.data.EEG.data_lrp_aver = array2table(EEG_data.Data.data.EEG.data_lrp_aver);
+        EEG_data.Data.data.EEG.data_lrp_aver(:,3) = id;
 
-    ms = [ms_start:2:ms_end]'; %create ms vector with time steps of 2 ms
+        clear id
+        
+        %add column names
+        colNames={'amplitude', 'ms', 'id'};
+        EEG_data.Data.data.EEG.data_lrp_aver.Properties.VariableNames = colNames;
     
-    EEG_data.Data.data.EEG.data_lrp_aver(:,2)=ms;
-    
-    %add vector with id
-    id = cell2table(part_code);
-    id = repmat(id,length(ms),1);
-    EEG_data.Data.data.EEG.data_lrp_aver = array2table(EEG_data.Data.data.EEG.data_lrp_aver);
-    EEG_data.Data.data.EEG.data_lrp_aver(:,3) = id;
-
-    clear id
-    
-    %add column names
-    colNames={'amplitude', 'ms', 'id'};
-    EEG_data.Data.data.EEG.data_lrp_aver.Properties.VariableNames = colNames;
-    
-    %create one output file for all participants
-    %for correct responses
-    if exist('data_lrp_aver', 'var') == 0
-       data_lrp_aver = EEG_data.Data.data.EEG.data_lrp_aver; %create new if it doesn't exist
-    else 
-       if ismember(part_code, data_lrp_aver.id) == 0 %only append when current part has not been appended 
-       data_lrp_aver = vertcat(data_lrp_aver, EEG_data.Data.data.EEG.data_lrp_aver); %append if it exists
-       end
-    end
+        %create one output file for all participants
+        if exist('data_lrp_aver', 'var') == 0
+           data_lrp_aver = EEG_data.Data.data.EEG.data_lrp_aver; %create new if it doesn't exist
+        else 
+           if ismember(part_code, data_lrp_aver.id) == 0 %only append when current part has not been appended 
+           data_lrp_aver = vertcat(data_lrp_aver, EEG_data.Data.data.EEG.data_lrp_aver); %append if it exists
+           end
+        end
+        
         clear EEG_data
-    
+    end
 end
 
 %Save output files
 out_file = [output_dir 'data_lrp_aver.mat'];
 save(out_file, 'data_lrp_aver');
-
-
-%     % create substructures for responsetypes (dp x trials)
-%     EEG_data.Data.data.EEG.data_lrp_correct = EEG_data.Data.data.EEG.data_lrp(:, events_response.responsetype == 1);
-%     EEG_data.Data.data.EEG.data_lrp_error = EEG_data.Data.data.EEG.data_lrp(:, events_response.responsetype == 2);
-%     
-%     %only continue if data set contains any error trials
-%     if isempty(EEG_data.Data.data.EEG.data_lrp_error)
-%         fprintf('There are no error trials in the data set. Continuing with next participant.');
-%         ErrorMessage = "Zero error trials.";
-%         FileName = strcat(output_dir, 'logs/', "Error_", part_code, ".txt");
-%         fid = fopen(FileName, 'wt');
-%         fprintf(fid, ErrorMessage);
-%         fclose(fid);
-%     else
-%         %create participant average across trials (dp)
-%         %for correct responses
-%         EEG_data.Data.data.EEG.data_lrp_correct_aver = zeros(length(EEG_data.Data.data.EEG.data_lrp_correct(:,1)),1);
-%         for iii = 1:length(EEG_data.Data.data.EEG.data_lrp_correct(:,1))
-%             EEG_data.Data.data.EEG.data_lrp_correct_aver(iii)=mean(EEG_data.Data.data.EEG.data_lrp_correct(iii,:));
-%         end
-%         clear iii
-%         
+ 
 %         %for errors
 %         EEG_data.Data.data.EEG.data_lrp_error_aver = zeros(length(EEG_data.Data.data.EEG.data_lrp_error(:,1)),1);
-%         for iii = 1:length(EEG_data.Data.data.EEG.data_lrp_error(:,1))
-%             EEG_data.Data.data.EEG.data_lrp_error_aver(iii)=mean(EEG_data.Data.data.EEG.data_lrp_error(iii,:));
+%         for i = 1:length(EEG_data.Data.data.EEG.data_lrp_error(:,1))
+%             EEG_data.Data.data.EEG.data_lrp_error_aver(i) = mean(EEG_data.Data.data.EEG.data_lrp_error(i,:));
 %         end
-%         clear iii
-%         
-%         %add vector with ms
-%         ms_start = EEG_data.Data.data.EEG.xmin*1000; %start of epoch 
-%         ms_end = EEG_data.Data.data.EEG.xmax*1000;  %end of epoch
-% 
-%         % add code for sampling rate 512 Hz here, something like
-%         % if sampling == 512
-%             % ms_down = [ms_start:1.95:ms_end]'; 
-%             % ms_base =  [ms_start:2:ms_end]';
-%             % ms_base = [ms_base, zeroes(length(ms_down)-length(ms_base))]';
-%             % ms = [ms_down, ms_base];
-%             % ms.diff = ms.down-ms.base;
-%             % for i = 1:length(ms.down)
-%             % if ms.diff(i) > 1
-%                 % ms.down(i)=[];
-%                 % ms.diff = ms.down-ms.base;
-%             % end
-%             %end
-%             %else 
-%             %...
-%             %end
-% 
-%         ms = [ms_start:2:ms_end]'; %create ms vector with time steps of 2 ms
+%         clear i
+%     
 %         
 %         EEG_data.Data.data.EEG.data_lrp_correct_aver(:,2)=ms;
 %         EEG_data.Data.data.EEG.data_lrp_error_aver(:,2)=ms;
 %         
-%         %add vector with id
-%         id = cell2table(part_code);
-%         id = repmat(id,length(ms),1);
 %         EEG_data.Data.data.EEG.data_lrp_correct_aver = array2table(EEG_data.Data.data.EEG.data_lrp_correct_aver);
 %         EEG_data.Data.data.EEG.data_lrp_correct_aver(:,3) = id;
 %         EEG_data.Data.data.EEG.data_lrp_error_aver = array2table(EEG_data.Data.data.EEG.data_lrp_error_aver);
 %         EEG_data.Data.data.EEG.data_lrp_error_aver(:,3) = id;
-%         
-%         clear id
 %     
-%         %add column names
-%         colNames={'amplitude', 'ms', 'id'};
+%        
 %         EEG_data.Data.data.EEG.data_lrp_correct_aver.Properties.VariableNames = colNames;
 %         EEG_data.Data.data.EEG.data_lrp_error_aver.Properties.VariableNames = colNames;
 %     
-%         %create one output file for all participants
-%         %for correct responses
-%         if exist('data_lrp_correct_aver', 'var') == 0
-%            data_lrp_correct_aver = EEG_data.Data.data.EEG.data_lrp_correct_aver; %create new if it doesn't exist
-%         else 
-%            if ismember(part_code, data_lrp_correct_aver.id) == 0 %only append when current part has not been appended 
-%            data_lrp_correct_aver = vertcat(data_lrp_correct_aver, EEG_data.Data.data.EEG.data_lrp_correct_aver); %append if it exists
-%            end
-%         end
-%     
-%         %for errors
 %         if exist('data_lrp_error_aver', 'var') == 0
-%            data_lrp_error_aver = EEG_data.Data.data.EEG.data_lrp_error_aver;
+%            data_lrp_error_aver = EEG_data.Data.data.EEG.data_lrp_error_aver; %create new if it doesn't exist
 %         else 
-%            if ismember(part_code, data_lrp_error_aver.id) == 0
-%             data_lrp_error_aver = vertcat(data_lrp_error_aver, EEG_data.Data.data.EEG.data_lrp_error_aver);
+%            if ismember(part_code, data_lrp_error_aver.id) == 0 %only append when current part has not been appended 
+%            data_lrp_error_aver = vertcat(data_lrp_error_aver, EEG_data.Data.data.EEG.data_lrp_error_aver); %append if it exists
 %            end
 %         end
-%     
-%         clear EEG_data
-% 
-%     end
-%     
-% end
-% 
-% %Save output files
+%         
 % out_file_correct = [output_dir 'data_lrp_correct_aver.mat'];
 % save(out_file_correct, 'data_lrp_correct_aver');
 % 
