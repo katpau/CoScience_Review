205d0887fdbfd6d0c87855f1e2b0eabc27b577fe
eporth <elisa.porth@uni-koeln.de>
2023-06-15 11:18:40 +0200

    Update prep_mvpa: common channels, sampling rate
    Ensures that there are 59 channels in the data set and "downsamples" data from 512 to 500 Hz

--- Analysis_Functions/Error_MVPA/MVPA/01_Preprocessing/prep_mvpa.m
+++ Analysis_Functions/Error_MVPA/MVPA/01_Preprocessing/prep_mvpa.m
@@ -92,7 +92,7 @@ end
 common_channels = {'FP1', 'FP2', 'AF7', 'AF8', 'AF3', 'AF4', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'FT7', 'FT8', ...
                    'FC5', 'FC6', 'FC3', 'FC4', 'FC1', 'FC2', 'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'T7', 'T8', 'TP7', 'TP8', ...
                    'CP5', 'CP6', 'CP3', 'CP4', 'CP1', 'CP2', 'P1', 'P2', 'P3', 'P4', 'P5', 'P6', 'P7', 'P8', 'PO7', 'PO8', ...
-                   'PO3', 'PO4', 'O1', 'O2', 'OZ', 'POZ', 'PZ', 'CPZ', 'CZ', 'FCZ', 'FZ', 'AFZ', 'FPZ'}';
+                   'PO3', 'PO4', 'O1', 'O2', 'OZ', 'POZ', 'PZ', 'CPZ', 'CZ', 'FCZ', 'FZ'}'; %from the 61 common electrodes, AFZ and FPZ were used as grounds, so they are not included here
 common_channels = cell2table(common_channels);
 chanlocs_part = struct2table(EEG_data.Data.data.EEG.chanlocs); %chanlocs of participant
 rm_chan = ~ismember(lower(chanlocs_part.labels), lower(common_channels.common_channels)); %find which channels need to be removed
@@ -100,8 +100,40 @@ rm_chan_names = chanlocs_part.labels(rm_chan); %get names of channels that need 
 EEG_data.Data.data.EEG.chaninfo.additional_chans_removed = rm_chan_names;
 
 EEG_data.Data.data.EEG = pop_select(EEG_data.Data.data.EEG, 'nochannel', rm_chan_names); %remove channels from EEG structure
-
+if length(EEG_data.Data.data.EEG.data(:,1,1)) ~= size(common_channels)
+    fprintf('Results of sanity check: \n The number of channels does not equal the requested number of common channels.');
+    ErrorMessage = "Wrong number of channels.";
+    FileName = strcat(SLIST.output_dir, 'logs/', "Error_", info.participant, ".txt");
+    fid = fopen(FileName, 'wt');
+    fprintf(fid, ErrorMessage);
+    fclose(fid);
+    return
+end
 clear common_channels chanlocs_part rm_chan rm_chan_names
+
+%for 512Hz sampling rate, remove additional measuring points to fit with
+%500Hz sampling rate
+if EEG_data.Data.data.EEG.srate == 512
+    ms_down = [window_start:1.95:window_end-1]'; 
+    ms_base =  [window_start:2:window_end-1]';
+    ms_base = vertcat(ms_base, zeros(length(ms_down)-length(ms_base),1));
+    ms = [ms_down, ms_base];
+    ms = array2table(ms);
+    ms.Properties.VariableNames = ["down","base"];
+    ms.diff = ms.down-ms.base;
+    ms.del = zeros(length(ms_base),1);
+    for i = 1:length(ms.down)
+        if ms.diff(i) >= 1 || ms.diff(i) <= -1
+            ms.del(i) = 1;
+            ms.down = vertcat(ms.down(1:i-1), ms.down(i+1:end), 0);
+            ms.diff = ms.down-ms.base;
+        end
+    end
+% EEG_data.Data.data.EEG.data_lrp_aver = array2table(EEG_data.Data.data.EEG.data_lrp_aver);
+EEG_data.Data.data.EEG.data(:,find(ms.del),:) = [];
+end
+
+clear ms_base ms_down ms
 
 %% Create EEG substructures from 3d EEGmatrix containing only trials from the respective responsetype (dp x channels x trials)
 
