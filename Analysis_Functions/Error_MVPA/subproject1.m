bb5f6887ca471cce61f871e2e6b1f522ba2127f1
eporth <elisa.porth@uni-koeln.de>
2023-07-20 09:35:00 +0200

    Subproject1 now includes only 2nd level analyses
    The first level lrp- and mvpa- analyses were added to the step-function TimeWindow, so that subproject1 now only includes the second level analyses

--- Analysis_Functions/Error_MVPA/subproject1.m
+++ Analysis_Functions/Error_MVPA/subproject1.m
@@ -1,15 +1,8 @@
-%% This scripts runs all lrp and mvpa analyses and compares the lrp and mvpa onsets
+%% This scripts compares the lrp and mvpa onsets
 
 %% Specifications 
-global AnalysisName;
-AnalysisName = "GoNoGo_MVPA"; %or "Flanker_MVPA" 
-
-%Preprocessing MVPA
-first_part = 1; %index of first participant in folder to analyse
-last_part = 2; %index of last participant in folder to analyse 
-
-%Main analyses MVPA
-participants = [1:2]; % index of participants in PreprocessedData to analyse
+global AnalysisName
+AnalysisName = "Flanker_MVPA"; %or "GoNoGo_MVPA" 
 
 %% Add Relevant Paths 
 global bdir;
@@ -18,41 +11,13 @@ bdir = pwd; % Base directory
 addpath(genpath(strcat(bdir, "/Only_ForGit_to_TestRun/")))
 addpath(genpath(strcat(bdir, "/Analysis_Functions/Error_MVPA/")))
 
-% %% LRP
-% % Preprocessing 
-% prep_lrp
-% 
-% % Jackknifing
-% jackknifing_lrp
-% 
-% %% MVPA
-% % Preprocessing
-% % run for all specified participants
-% 
-% for part = first_part:last_part   
-%     try
-%         prep_mvpa(part)      
-%     catch 
-%         fprintf('Participant %d does not exist \n', part)   
-%     end    
-% end
-% 
-% % First-level Analyses
-% error = 1;
-% for part = participants
-%     for group = 1
-%         try
-%             DECODING_ERP('coscience', 1, 0, part, group, 0);
-%         catch
-%             protocol(error, 1) = part;
-%             protocol(error, 2) = group;
-%             error = error + 1;
-%         end 
-%     end 
-% end 
-% 
-% % Second-level Analyses
-% ANALYSE_DECODING_ERP('coscience',1,0,'all',1)
+%% LRP
+% Jackknifing
+jackknifing_lrp
+
+%% MVPA
+% Second-level Analyses
+ANALYSE_DECODING_ERP('coscience',0,'all',1)
 
 %% Compare lrp and mvpa onsets
 %load lrp data
@@ -67,9 +32,9 @@ load(filename_lrp);
 
 %load mvpa data
 if strcmp(AnalysisName, "Flanker_MVPA")
-      input_dir_mvpa = [bdir '\Analysis_Functions\Error_MVPA\MVPA\02_MVPA\DECODING_RESULTS\level_1\flanker\'];   
+      input_dir_mvpa = [bdir '\Analysis_Functions\Error_MVPA\MVPA\02_MVPA\DECODING_RESULTS\level_2\flanker\'];   
  elseif strcmp(AnalysisName, "GoNoGo_MVPA") 
-    input_dir_mvpa = [bdir '\Analysis_Functions\Error_MVPA\MVPA\02_MVPA\DECODING_RESULTS\level_1\go_nogo\'];  
+    input_dir_mvpa = [bdir '\Analysis_Functions\Error_MVPA\MVPA\02_MVPA\DECODING_RESULTS\level_2\go_nogo\'];  
 end
 
 filename_mvpa = [input_dir_mvpa, 'all_part_acc.mat'];
