5d39608d10b7f96183dca7c917d502de8e279256
eporth <elisa.porth@uni-koeln.de>
2023-10-17 15:36:46 +0200

    Update TimeWindow for LRP without CSD
    use non-transformed data for LRP

--- Step_Functions/Quantification_MVPA/TimeWindow.m
+++ Step_Functions/Quantification_MVPA/TimeWindow.m
@@ -43,6 +43,7 @@ try
     %%%%%%%%%%%%%%%% Routine for the analysis of this step
     % This functions starts from using INPUT and returns OUTPUT
     EEG = INPUT.data.EEG;
+    LRP = INPUT.data.LRP;
     
     % Condition Names and Triggers depend on analysisname
     if INPUT.AnalysisName == "Flanker_MVPA"
@@ -58,10 +59,16 @@ try
     Event_Window = [-0.300 0.300]; % Epoch length in seconds
     NrConditions = length(Condition_Names);
     
-    % Loop Through the conditions like this?
+    % epoch data
     for i_Cond = 1:NrConditions
         (Condition_Names(i_Cond))
         pop_epoch(EEG, Condition_Triggers(i_Cond,:), Event_Window, 'epochinfo', 'yes');     
+    end
+
+   % epoch lrp data
+   for i_Cond = 1:NrConditions
+        (Condition_Names(i_Cond))
+        pop_epoch(LRP, Condition_Triggers(i_Cond,:), Event_Window, 'epochinfo', 'yes');     
     end
     
     % run first-level lrp and mvpa analyses    
@@ -114,11 +121,11 @@ try
     ms_end = 299;
     
     % Create EEG substructures from 3d EEGmatrix containing only trials from LRP electrodes (channels x dp x trials)
-    chanlocs = struct2table(EEG.chanlocs);
+    chanlocs = struct2table(LRP.chanlocs);
     Electrodes = upper(strsplit(OUTPUT.StepHistory.Electrodes, ", ")); 
     lrp_chanlocs = [find(strcmp(chanlocs.labels, Electrodes(1))) find(strcmp(chanlocs.labels, Electrodes(2)))]; %only use LRP electrodes of interest
     
-    EEG_lrp = pop_select(EEG, 'channel', lrp_chanlocs); %remove other channels from EEG structure
+    EEG_lrp = pop_select(LRP, 'channel', lrp_chanlocs); %remove other channels from EEG structure
     
     clear chanlocs lrp_chanlocs
     
