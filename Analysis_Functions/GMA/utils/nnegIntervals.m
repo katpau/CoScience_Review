87f3bd7adaec97530c04f26ff885b68bdbd5c556
0xlevel <0xlevel@gmail.com>
2023-11-13 11:26:48 +0100

    Changed closed flag from numeric to logical.

--- Analysis_Functions/GMA/utils/nnegIntervals.m
+++ Analysis_Functions/GMA/utils/nnegIntervals.m
@@ -6,31 +6,38 @@
 %   [xIv, xClose] = nnegIntervals(x, false);
 %
 %% Description
-%   Returns the x-axis intervals between intercepts (including zero), i.e.
-%   all returned y values within the interval(s) are nonnegative.
+%   Returns the x-axis intervals between intercepts (including zero values),
+%   i.e., all returned y values within the interval(s) are nonnegative.
 %
-%   With default parameters, the function returns open and closed intervals. A
-%   nonnegative interval is open, if it begins or ends with the first or last
-%   element of y, which is not positive.
-%
-%   However, if all values are nonnegative and the first and the last y are 0,
-%   the interval is also considered as closed.
+%   With default parameters, the function returns open and closed intervals.
+%   Closed intervals are nonnegative values of y, enclosed by negative values.
+%   As a special case, a vector of only nonnegative values, will be searched for
+%   positive intervals, enclosed by zero values, if srcPositive is set to true.
+%   A vector of identical nonnegative values will always be treated as a single
+%   open interval.
+%   All y values will be rounded to their significant decimal before processing
+%   to avoid future rounding errors.
 %
 %% Input
 %   y           - [numeric] vector of real numbers
 %   inclOpen    - [logical] true = (default) include open intervals of
-%               nonnegative values with a single x-intercept;
+%               nonnegative values with none or a single x-intercept;
 %               false = only include closed intervals between two
-%               x-intercepts
+%               x-intercepts (see srcPositive for pure nonnegative data).
+%   srcPositive - [logical] true = (default) Allows to search for positive
+%               intervals, in case all values in y are nonnegative.
+%               If set to false, vector of nonnegative values will yield the
+%               whole vector as interval, if inclOpen is tue or no interval, if
+%               inclOpen is false.
 %
 %% Output
-%   iv          - [numerical {integer}] start and end indices (rows) for each 
+%   iv          - [numerical {integer}] start and end indices (rows) for each
 %               nonnegative interval (column); if inclOpen is true, it may
 %               include open intervals
 %   closed      - [logical] flag for each interval (column index of iv):
 %               true = closed interval, false = open interval;
-%               Returns empty, if no intervals were found. Returns 0 if all
-%               values are nonnegative. Returns 1, if all values are positive,
+%               Returns empty, if no intervals were found. Returns false if all
+%               values are nonnegative. Returns true, if all values are positive,
 %               but 0 in the first and the last.
 %
 %% Examples
@@ -46,7 +53,7 @@
 %   seg = y1iv(:, maxidx);
 
 %% Attribution
-%	Last author: Olaf C. Schmidtmann, last edit: 26.06.2023
+%	Last author: Olaf C. Schmidtmann, last edit: 24.08.2023
 %   Source: https://github.com/0xlevel/gma
 %	MATLAB version: 2023a
 %
@@ -55,45 +62,62 @@
 %   it under the terms of the GNU General Public License as published by
 %   the Free Software Foundation, either version 3 of the License, or
 %   (at your option) any later version.
-% 
+%
 %   This program is distributed in the hope that it will be useful,
 %   but WITHOUT ANY WARRANTY; without even the implied warranty of
 %   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 %   GNU General Public License for more details.
 
-function [iv, closed] = nnegIntervals(y, inclOpen)
+%% Todo
+%   - Refactor: extract search inside positive values and abstract both search
+%     types
+
+function [iv, closed] = nnegIntervals(y, inclOpen, srcPositive)
 
     arguments
         y(1, :) {mustBeNumeric}
         inclOpen(1, 1) logical = true
+        srcPositive(1, 1) logical = true
     end
 
-    nY = length(y);
-    y = real(y);
+    iv = [];
+    closed = false(0);
+
+    % EARLY return, if empty, flat or all negative
+    if isempty(y) || all(y < 0) || all(y(1) == y), return; end
 
     % Round y to the significant decimal.
+    y = real(y);
     dsig = abs(ceil(log10(eps(max(abs(y))))));
     y = round(y, dsig);
 
     yp = y >= 0;
+    nY = length(y);
 
-    iv = [];
-    closed = [];
+    if all(yp)
+        % Special case: all values are nonnegative
+        if srcPositive && sum(y == 0) > 2
 
-    %% Special case, early return: all values of y are nonnegative.
-    % Always returns one interval for the full data, even if it is not closed.
-    % If the first and the last values of y are zero, the interval is marked as
-    % closed; otherwise it is open.
+            % Mark positive intervals, only
+            signChg = [0, diff(y > 0)];
 
-    if inclOpen && all(yp)
-        iv = [1; nY];
-        closed = y(1) == 0 & y(nY) == 0;
-        return;
-    elseif all(~yp)
-        return;
+        else
+            if inclOpen
+                iv = [1; nY];
+                closed = false;
+            end
+            return;
+        end
+
+    else
+        % Mark nonnegative intervals, first zero is marked (special case)
+        if inclOpen
+            signChg = [0, diff(yp)];
+        else
+            signChg = diff([y(1) ~= 0, yp]);
+        end
     end
 
-    signChg = [0, diff(yp)];
     icpt = find(signChg);
     swing = signChg(icpt);
 
@@ -132,7 +156,7 @@ function [iv, closed] = nnegIntervals(y, inclOpen)
     iv = reshape(icpt, 2, []);
     iv(2, :) = iv(2, :) - 1;
     niv = size(iv, 2);
-    closed = ones(1, niv);
+    closed = true(1, niv);
     ivrng = [1, niv];
-    closed(ivrng(isOpen)) = 0;
+    closed(ivrng(isOpen)) = false;
 end
