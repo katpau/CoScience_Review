5c60334ad86fe085e704ebd4d56d5381cd63ac41
pbierwirth <philipp.bierwirth@uni-marburg.de>
2024-03-08 16:04:59 +0100

    Add ECG2IBI Functions for CECT Analysis
    Added custom-made ECG2IBI functions to Analysis_Functions module. These functions are designed to:
    
    - Detect R peaks in the ECG data.
    
    - Calculate a continuous Interbeat Interval (IBI) channel, effectively creating a heart period channel.
    
    - Detect and remove artifacts in the IBI data.
    
    - Calculate Cardio-Electroencephalographic Covariance Tracing (CECT), enhancing the analysis precision.
    
    Furthermore, these functions have been integrated into the existing quantification procedure for analyzing the N300H component of CECT.

--- Step_Functions/Quantification_N300H/Quantification_N300H.m
+++ Step_Functions/Quantification_N300H/Quantification_N300H.m
@@ -1,12 +1,12 @@
-function  OUTPUT = Quantification_ERP(INPUT, Choice)
-% Last Checked by KP 12/22
+function  OUTPUT = Quantification_N300H(INPUT, Choice)
+% Last Checked by
 % Planned Reviewer:
-% Reviewed by: 
+% Reviewed by:
 
 % This script does the following:
 % Based on information of previous steps, and depending on the forking
 % choice, N300H is calculated and Exported.
-% Script also extracts Measurement error and reshapes Output to be easily
+% Script also reshapes Output to be easily
 % merged into a R-readable Dataframe for further analysis.
 %#####################################################################
 %### Usage Information                                         #######
@@ -38,205 +38,249 @@ SaveInterim = logical([1]);
 Order = [24];
 
 
+
 %%%%%%%%%%%%%%%% Updating the SubjectStructure. No changes should be made here.
 INPUT.StepHistory.Quantification_ERP = Choice;
 OUTPUT = INPUT;
 OUTPUT.data = [];
+
 % Some Error Handling
 try
     %%%%%%%%%%%%%%%% Routine for the analysis of this step
+
     EEG = INPUT.data.EEG;
     ECG = INPUT.ECG;
-    
-    % Info on ** Epochs **
-    Event_Window = [-0.200 2];
-    Event_WindowECG = [-0.5 5];
-    Condition_Triggers =  {100, 110, 150; 101, 111, 151};
-    Condition_Names = ["Loss", "Win"];
-    NrConditions = length(Condition_Names);
-    
-    
-    % ********************************************************************************************
-    % **** Set Up Some Information for EEG *******************************************************
-    % ********************************************************************************************
+
     % Get Info on ** Electrodes **
     Electrodes = strrep(upper(strsplit(INPUT.StepHistory.Electrodes , ",")), " ", "");
     NrElectrodes = length(Electrodes);
     ElectrodeIdx = findElectrodeIdx(EEG.chanlocs, Electrodes);
-    
-    % ****** Get Info on TimeWindow EEG ******
-    TimeWindow_EEG = INPUT.StepHistory.TimeWindow;
-    TimeWindow_EEG = str2double(strsplit(TimeWindow_EEG, ","));
-    if ~contains(TimeWindow_EEG, "Relative")
-        TimeWindow_EEG = str2double(strsplit(TimeWindow_EEG, ","));
+
+    %% Calculate CECTs
+
+    % Define settings for CECT analysis
+    cfg.segsizeIBI = [-0.5, 5];
+    cfg.segsizeEEG = [-0.2, 2];
+    cfg.ibibins = [0, 5];
+    cfg.eegbins = [0, 1];
+    cfg.srateEEG = EEG.srate;
+    cfg.srateIBI = ECG.srate;
+    cfg.ibibinsize = 500;
+    cfg.eegbinsize = 10;
+
+    % ***********************************************************************************
+    % calculate CECTs for each condition ************************************************
+    % ***********************************************************************************
+
+    % prepare markers and triggers
+    Condition_Names = {'no_loss', 'medium_loss', 'high_loss', ...
+        'no_win', 'medium_win', 'high_win' };
+    Condition_Triggers = {'200' '100'; '210' '110';'250' '150';...
+        '201' '101'; '211' '111';'251' '151'}';
+    NrConditions = length(Condition_Names);
+
+
+    % Check if electrodes are averaged before extracting the N300H,
+    % i.e. before calculating intra-individual correlations:
+    if strcmp(INPUT.StepHistory.Cluster_Electrodes,"cluster")
+        EEG = pop_select(EEG, 'channel', ElectrodeIdx);
+        % Calculate average of electrodecluster and save it as channel 1 in
+        % EEG.data matrix
+        EEG.data(1,:,:) = squeeze(mean(EEG.data,1));
+        % change value of relevant channel containing the average of the
+        % elctrode cluster
+        ElectrodeIdx = 1;
+        NrElectrodes = 1;
+        Electrodes = append('cluster: ', join(Electrodes));
+    end
+
+    for i = 1:size(Condition_Triggers,2)
+
+        % find index of relevant condition triggers within the EEG.event structure
+        idx = []; idx=find(contains({EEG.event.type}, Condition_Triggers(:,i)));
+
+        % get number of epochs
+        Epochs(i,1) = length(idx);
+
+        % calculate CECTS via CECT.m and save results for each condition in
+        % a "cect results" structure
+        cect.results.(Condition_Names{i}) = ...
+            CECT(EEG.data(:,:,idx), squeeze(ECG.data(2,:,idx)), cfg);
+
+    end
+    % save the employed CECT settings into the cect results structure
+    cect.settings = cfg;
+
+    % ****** Get Info on TimeWindow of EEG and IBI data ******
+    % Get choices for the time window of interest in which the N300H is defined
+
+    % For the IBI data
+    if ~contains(INPUT.StepHistory.ECG_TimeWindow ,"Relative_Group")
+        IBI_window = [str2num(INPUT.StepHistory.ECG_TimeWindow)];
+        % Translate given time windows from ms into CECT bins
+        % IBI_window as well as the ibi- and eegbin size are given in ms
+        BinsIBI = IBI_window/cfg.ibibinsize; BinsIBI(1) = BinsIBI(1)+1;
     else
-        % Time window needs to be longer since peak could be at edge
-        % this part of the data will be exported later
-        TimeWindow_EEG = [150 500];
+        IBI_window = [1500 5500];
+        % Translate given time windows from ms into CECT bins
+        % IBI_window as well as the ibi- and eegbin size are given in ms
+        BinsIBI = IBI_window/cfg.ibibinsize; BinsIBI(1) = BinsIBI(1)+1;
     end
-    
-    % Get Index of TimeWindow  [in Sampling Points]
-    TimeIdx_EEG = findTimeIdx(EEG.times, TimeWindow_EEG(1), TimeWindow_EEG(2));
-    
-    
-    
-    % ********************************************************************************************
-    % **** Set Up Some Information ECG *************************************************************
-    % ********************************************************************************************
-    % ****** Get Info on TimeWindow ECG ******
-    TimeWindow_ECG = INPUT.StepHistory.ECG_TimeWindow;
-    TimeWindow_ECG = str2double(strsplit(TimeWindow_ECG, ","));
-    if ~contains(TimeWindow_EEG, "Relative")
-        TimeWindow_ECG = str2double(strsplit(TimeWindow_ECG, ","));
+
+    % For the EEG data
+    if ~contains(INPUT.StepHistory.TimeWindow ,"Relative_Group")
+
+        EEG_window = [str2num(INPUT.StepHistory.TimeWindow)];
+
+        % Translate given time windows from ms into CECT bins
+        % IBI_window as well as the ibi- and eegbin size are given in ms
+        BinsEEG = EEG_window/cfg.eegbinsize; BinsEEG(1) = BinsEEG(1)+1;
     else
-        % Time window needs to be longer since peak could be at edge
-        % this part of the data will be exported later
-        TimeWindow_ECG = [1500 5500];
+
+        EEG_Window = [150 500];
+
+        % Translate given time windows from ms into CECT bins
+        % IBI_window as well as the ibi- and eegbin size are given in ms
+        BinsEEG = EEG_window/cfg.eegbinsize; BinsEEG(1) = BinsEEG(1)+1;
     end
-    
-    % Get Index of TimeWindow  [in Sampling Points]
-    TimeIdx_ECG = findTimeIdx(ECG.times, TimeWindow_ECG(1), TimeWindow_ECG(2));
-    
+
+
     % ********************************************************************************************
-    % ****  Prepare EEG & ECG Data *************************************************************************
+    % **** Extract N300H Data and prepare Output Table    **********************************************
     % ********************************************************************************************
-    for i_Cond = 1:NrConditions
-        % * Epoch Data around predefined window and save each
-        EEGData = pop_epoch( EEG, Condition_Triggers(i_Cond,:), Event_Window, 'epochinfo', 'yes');
-        ECGData = pop_epoch( ECG, Condition_Triggers(i_Cond,:), Event_WindowECG, 'epochinfo', 'yes');
-        
-        % Extract only relevant TimeWindow
-        EEG.(Condition_Names(i_Cond)) = EEGData.data(ElectrodeIdx, TimeIdx_EEG,:);
-        ECG.(Condition_Names(i_Cond)) = ECGData.data(TimeIdx_ECG,:);
-        
-        % Update Nr on Electrodes for Final OutputTable if Clustered before calculating N300H
-        if INPUT.StepHistory.Cluster_Electrodes == "cluster"
-            NrElectrodes = 1;
-            Electrodes = strcat('Cluster ', join(Electrodes));
-            EEG.(Condition_Names(i_Cond)) = mean(EEG.(Condition_Names(i_Cond)),1);
-        end
-    end
-    
-    
-    % ********************************************************************************************
-    % **** Extract Data and prepare Output Table    **********************************************
-    % ********************************************************************************************
+
+    % ****** Get Info on N300H extraction method of EEG and IBI data ******
+    % Get choices for the time window of interest in which the N300H is defined
+
     % if any Time window is dependent on group, do not continue but save
     % relevant data
-    if contains(INPUT.StepHistory.TimeWindow, "Relative_Group") || contains(INPUT.StepHistory.ECG_TimeWindow, "Relative_Group"
+    if contains(INPUT.StepHistory.TimeWindow, "Relative_Group") || ...
+            contains(INPUT.StepHistory.ECG_TimeWindow, "Relative_Group")
         For_Relative.RecordingLab = EEG.Info_Lab.RecordingLab;
         For_Relative.Experimenter = EEG.Info_Lab.Experimenter;
         For_Relative.DataEEG = EEG;
         For_Relative.DataECG = ECG;
         For_Relative.Times = EEGData.times(TimeIdx_EEG);
         For_Relative.Electrodes = Electrodes;
-        
-        
-    else
-        % ****** Extract N300H ******
-        
-        % ****** Create BINS ******
-        % EEG: ca. 10 ms (depending on sampling rate); ECG: ca. 500 ms (depending on sampling rate)
-        
-        % ****** Calculate Correlation ******
-        % Fisher Z-transformed lagged Pcorrelation (Pearson’s r)
-        % N300H = atanh(r)?????
-        
-        % ****** Export N300H ******
-        if Choice == "Bins"
-            NrBins = 2; % ???
-            Bin_L = num2str(1:NrBins);
-        else
-            NrBins = 1;
-            Bin_L = "one";
-        end
-        
-        InitSize = [NrElectrodes,NrConditions, NrBins]; %
-        EpochCount = NaN(InitSize);
-        ERP =NaN(InitSize); SME=NaN(InitSize);
-        
-        
-        for i_Cond = 1:NrConditions
-            Data = ConditionData.(Condition_Names{i_Cond});
-            
-            % Count Epochs
-            EpochCount(:,i_Cond,:) = size(Data,3);
-            if size(Data,3) < str2double(INPUT.StepHistory.Trials_MinNumber)
-                ERP(:,i_Cond,:) = NaN;
-                SME(:,i_Cond,:) = NaN;
-            else
-                
-                
-                
-                
-                % Calculate Mean across Bins
-                if strcmp(Choice, "Mean")
-                    ERP(:,i_Cond,:) = mean(N300H,3);
-                    % SME(:,i_Cond,1) = Mean_SME(N300H); ?? how to get SME?
-                    
-                    % Export each Bin
-                elseif strcmp(Choice, "Bins")
-                    ERP(:,i_Cond,:) = N300H;
-                    % SME(:,i_Cond,1) = Mean_SME(N300H); ??
-                    
-                    % Take Maximum of Bin
-                elseif strcmp(Choice, "Maximum")
-                    ERP(:,i_Cond,:) = max(N300H,2);
-                    % SME(:,i_Cond,1) = Mean_SME(N300H); ??
-                end
+
+    else % Extract N300H
+        N300H = []; % electrodes x conditions x bins
+        for c = 1:NrConditions
+
+            % initialize CECT data matrix
+            tmp_data = NaN(1,BinsEEG(2)-BinsEEG(1)+1,BinsIBI(2)-BinsIBI(1)+1);
+            tmp_data = (cect.results. ...
+                (Condition_Names{1,c})...
+                (ElectrodeIdx, BinsEEG(1):BinsEEG(2), BinsIBI(1):BinsIBI(2)));
+
+            % Fisher-Z transform CECTs
+            tmp_data = atanh(tmp_data);
+
+            % Calculate mean across bins of interest
+            if  strcmpi(Choice,"Mean")
+                N300H(:,c,:) = mean(tmp_data,[2,3]);
+
+                % Export each bin
+            elseif strcmp(Choice, "Bins")
+                N300H(:,c,:) = squeeze(mean(tmp_data,3));
+
+                % detect maximal N300H within bins of interest
+            elseif strcmp(Choice, "Maximum")
+                % detecting the maximal N300H [negative correlation, therfore
+                % min()]
+                N300H(:,c,:) = min(tmp_data,[],[2,3]);
+
             end
         end
     end
-    
+
     % Update Nr on Electrodes for Final OutputTable if Clustered after N300H
     if INPUT.StepHistory.Cluster_Electrodes == "no_cluster_butAV"
         NrElectrodes = 1;
-        Electrodes = strcat('AV_of', join(Electrodes));
-        ERP = mean(ERP,1);
-        %SME = mean(SME,1);
+        Electrodes = append('AV_of: ', join(Electrodes));
+        N300H = mean(N300H,1);
     end
-    
-    
-    
-    
-    
-    
+
+    % ****** Export N300H ******
+    if Choice == "Bins"
+        NrBins = size(N300H,3);
+        Bin_L = num2cell(1:NrBins)';
+    else
+        NrBins = 1;
+        Bin_L = "one";
+    end
+
+    InitSize = [NrElectrodes,NrConditions, NrBins];
+    EpochCount = NaN(InitSize);
+    EpochCount =  repmat(Epochs', NrElectrodes, NrBins);
+
     % ********************************************************************************************
     % **** Prepare Output Table    ***************************************************************
     % ********************************************************************************************
-    
-    if contains(INPUT.StepHistory.TimeWindow, "Relative_Group") || contains(INPUT.StepHistory.ECG_TimeWindow, "Relative_Group")
+
+    if contains(INPUT.StepHistory.TimeWindow, "Relative_Group") || ...
+            contains(INPUT.StepHistory.ECG_TimeWindow, "Relative_Group")
         % Add Relative Data and Relative Info
         OUTPUT.data.For_Relative = For_Relative;
     else
         % Prepare Final Export with all Values
         % ****** Prepare Labels ******
-        % Subject, ComponentName, Lab, Experimenter is constant AAA
+        % Subject, ComponentName, Lab, Experimenter, Scoring is constant AAA
         Subject_L = repmat(INPUT.Subject, NrConditions*NrElectrodes*NrBins,1 );
         Lab_L = repmat(EEG.Info_Lab.RecordingLab, NrConditions*NrElectrodes*NrBins,1 );
         Experimenter_L = repmat(EEG.Info_Lab.Experimenter, NrConditions*NrElectrodes*NrBins,1 );
         Component_L = repmat("N300H", NrConditions*NrElectrodes*NrBins,1 );
-        
+        Scoring_L = repmat(Choice, NrConditions*NrElectrodes*NrBins,1 );
+        TimeWindow_L = repmat(num2str(EEG_window), NrConditions*NrElectrodes*NrBins, 1);
+        TimeWindowECG_L = repmat(num2str(IBI_window), NrConditions*NrElectrodes*NrBins, 1);
+
+
         % Electrodes: if multiple electrodes, they simply alternate ABABAB
         Electrodes_L = repmat(Electrodes', NrConditions*NrBins, 1);
-        
+
         % Conditions are blocked across electrodes, but alternate across
         % time windows AABBAABB
-        Conditions_L = repelem(Condition_Names', NrElectrodes*NrBins,1);
-        Conditions_L = repmat(Conditions_L(:), 1);
-        
-        % Time Window are blocked across electrodes and conditions AAAAABBBB
-        TimeWindow_L = repmat(num2str(TimeWindow_EEG), NrConditions*NrElectrodes*NrBins, 1);
-        TimeWindowECG_L = repmat(num2str(TimeWindow_ECG), NrConditions*NrElectrodes*NrBins, 1);
-        Bin_L = repmat(Bin_L, NrConditions*NrElectrodes, 1);
-        
-        
+        Conditions_L = repelem(Condition_Names', NrElectrodes,1);
+        Conditions_L = repmat(Conditions_L(:), NrBins,1);
+
+        % Bins are blocked across electrodes and conditions AAAAABBBB
+        Bin_L = repelem(Bin_L, NrConditions*NrElectrodes, 1);
+
+        %******************************************************************
+        % check if number of positive and negative feedback segments
+        % drop below the minimum number of segments. If segment number is
+        % smaller -> mark condition as outlier by setting it to NaN
+        %******************************************************************
+        if sum(EpochCount(contains(Conditions_L, 'loss'))) < ...
+                str2num(INPUT.StepHistory.Trials_MinNumber)
+
+            N300H(contains(Conditions_L, 'loss'),1) = NaN;
+
+        elseif  sum(EpochCount(contains(Conditions_L, 'win'))) < ...
+                str2num(INPUT.StepHistory.Trials_MinNumber)
+
+            N300H(contains(Conditions_L, 'win'),1) = NaN;
+        end
+
         % ****** Prepare Table ******
-        OUTPUT.data.Export = [cellstr([Subject_L, Lab_L, Experimenter_L, Conditions_L, Electrodes_L, TimeWindow_L, TimeWindowECG_L]),...
-            num2cell([ERP(:), SME(:), EpochCount(:)]), cellstr(Bin_L), cellstr(Component_L)];
-        
+        if Choice ~= "Bins"
+            NrBins = size(N300H,3);
+
+            OUTPUT.data.Export = [cellstr([Subject_L, Lab_L, Experimenter_L, Conditions_L, ...
+                Electrodes_L, TimeWindow_L, TimeWindowECG_L, Scoring_L]),...
+                num2cell([N300H(:),  EpochCount(:)]), cellstr(Bin_L), cellstr(Component_L)];
+        else
+
+            OUTPUT.data.Export = [cellstr([Subject_L, Lab_L, Experimenter_L, Conditions_L, ...
+                Electrodes_L, TimeWindow_L, TimeWindowECG_L, Scoring_L]),...
+                num2cell([N300H(:),  EpochCount(:)]), Bin_L(:), cellstr(Component_L)];
+        end
+
+        % save cect analsis for all conditions and electrodes
+        % (for plotting the temporal and spatial dynamics of the N300H)
+        OUTPUT.data.cect = cect;
     end
-    
+
     % ****** Error Management ******
 catch e
     % If error ocurrs, create ErrorMessage(concatenated for all nested
@@ -245,7 +289,6 @@ catch e
     for ierrors = 1:length(e.stack)
         ErrorMessage = strcat(ErrorMessage, "//", num2str(e.stack(ierrors).name), ", Line: ",  num2str(e.stack(ierrors).line));
     end
-    
+
     OUTPUT.Error = ErrorMessage;
-end
 end
--- Testing_Scripts/Test_Preproc_Erik.m
+++ Testing_Scripts/Test_Preproc_Erik.m
@@ -89,11 +89,12 @@ INPUT = Bad_Epochs(INPUT, "FASTER"); %  "FASTER", "Threshold_100", "Threshold_12
 
 INPUT = Reference(INPUT, "CAV"); %  "CAV"      "Mastoids"  "CSD" 
 INPUT = Baseline(INPUT, "-200 0"); %  "-200 0", "-100 0", 
-INPUT = TimeWindow(INPUT, "250,500"); %  "250,500"    "200,400"    "300,350"    "Relative_Group"
-INPUT = Cluster_Electrodes(INPUT, "no_cluster"); %    "no_cluster"    "cluster"
-INPUT = Electrodes(INPUT, "Cz"); % "Cz"    "Fz,FCz,Cz"    "Fcz,Cz"    "FCz"
-INPUT = ECG_Baseline(INPUT, "-500 0"); %  "-500 0"    "firstTP"
-INPUT = ECG_Artefact(INPUT, "remove"); %  "remove"    "interpolate"
-INPUT = ECG_TimeWindow(INPUT, "no_cluster"); % "2000,5000"    "3500,4000"    "Relative_Group"
-INPUT = Trials_MinNumber(INPUT, "51"); %  "51"    "68"
 
+INPUT = IBI_calculation(INPUT, "no interpolation"); % "no interpolation", interpolation 
+INPUT = IBI_epoching(INPUT, "-500 0"); % "-500 0"    "firstTP"
+INPUT = ECG_TimeWindow(INPUT, "2000,5000" );  % "2000,5000"    "3500,4000"    "Relative_Group"
+INPUT = TimeWindow(INPUT,  "250,500" ); %  "250,500"    "200,400"    "300,350"    "Relative_Group"
+INPUT = Electrodes(INPUT, "Cz"); % "Cz"    "Fz,FCz,Cz"    "Fcz,Cz"    "FCz"
+INPUT = Cluster_Electrodes(INPUT, "no_cluster"); %    "no_cluster"    "cluster" "no_cluster_butAV"
+INPUT = Trials_MinNumber(INPUT, "51"); %  "51"    "68"
+INPUT = Quantification_N300H(INPUT, "Mean"); % "Mean", "Bins", "Maximum"
